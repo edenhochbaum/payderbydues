42f451eb2b99404a696742cb535ed85a693858c1
Eden Hochbaum <eden.hochbaum@gmail.com>
2015-05-28 17:44:19 +0530

    Create DBConnect.pm

---
+++ perlmodules/PayDerbyDues/Utilities/DBConnect.pm
@@ -0,0 +1,16 @@
+package PayDerbyDues::Utilities::DBConnect;
+
+use strict;
+use warnings;
+
+use DBI;
+
+sub GetDBH {
+	return DBI->connect("dbi:Pg:dbname=$ENV{DBNAME};host=$ENV{DBHOST}", $ENV{DBUSERNAME}, $ENV{DBPASSWORD}, {
+		AutoCommit => 1,
+		RaiseError => 1,
+		PrintError => 1,
+	});
+}
+
+1;
