d4932939b9ea9bf0d1c0c41729bdc74f91938b09
edenhochbaum <eden@payderbydues.com>
2015-06-14 23:45:36 +0000

    Rewrite Auth::Middleware to use Router::Simple. So simple, such routing, wow.

--- perlmodules/Auth/Middleware.pm
+++ perlmodules/Auth/Middleware.pm
@@ -5,75 +5,84 @@ use warnings;
 
 use Plack::Request;
 use Plack::Response;
-
-my %routes;
+use Router::Simple;
 
 my %defconfig = (
     unauthredirect => '/',
     timeout_sec => 30 * 60,
     );
 
+sub wrap
+{
+    my ($app, %userconfig) = @_;
+
+    my %config = (%defconfig, %userconfig);
+    my $router = Router::Simple->new();
+    $router->connect('/login', { func => \&login }, { method => 'POST' });
+    $router->connect('/newuser', { func => \&newuser }, { method => 'POST' });
+
+    return sub {
+        my $env = shift;
+
+        require PathDerbyDues::Utilities::DBConnect;
+	my $dbh = PayDerbyDues::Utilities::DBConnect::GetDBH();
+
+	if (my $p = $router->match($env)) {
+	    my $req = Plack::Request->new($env);
+	    return $p->{func}->($req, $dbh, %config);
+	} elsif (!$config{authpaths} || $config{authpaths}->match($env)) {
+	    return dispatch($app, $env, $dbh, %config);
+        } else {
+            return $app->($env);
+        }
+    };
+}
+
 sub dispatch
 {
     my ($app, $env, $dbh, %config) = @_;
 
     my $req = Plack::Request->new($env);
-    if (exists $routes{$req->path}) {
-        return $routes{$req->path}->($req, $dbh, %config);
-    } elsif (exists $config{noauthpaths}->{$req->path}) {
-        return $app->($env);
-    } else {
-        my $s = $req->cookies->{s};
-        my $auth = Auth::Data->new($dbh);
-        my $username = $auth->check($s);
+    my $s = $req->cookies->{s};
+    my $auth = Auth::Data->new($dbh);
+    my $username = $auth->check($s);
 
-        if (!$username) {
-            my $res = Plack::Response->new;
-            $res->redirect($config{unauthredirect});
+    if (!$username) {
+        my $res = Plack::Response->new;
+        $res->redirect($config{unauthredirect});
 
-            return $res->finalize;
-        }
-
-        local $env->{_auth_username} = $username;
-        return $app->($env);
+        return $res->finalize;
     }
+
+    local $env->{_auth_username} = $username;
+    return $app->($env);
 }
 
 sub login
 {
     my ($req, $dbh, %config) = @_;
 
-    if ($req->method eq 'POST') {
-        my $auth = Auth::Data->new($dbh);
-        my $user = $req->query_parameters->{username};
-        my $pass = $req->query_parameters->{password};
-        my $token = $auth->auth($user, $pass);
-        my $res = Plack::Response->new;
-        
-        if (!$token) {
-            $res->redirect('/login?badpassword=1');
-            $res->cookies->{s} = '';
-            
-            return $res->finalize;
-        }
-        else {
-            my $redirect = _redirect_target($req, %config);
-            $res->redirect($redirect);
-            $res->cookies->{s} = {
-                value => $token,
-                expires => time + $config{timeout_sec},
-                secure => 1,
-            };
-            return $res->finalize;
-        }
+    my $auth = Auth::Data->new($dbh);
+    my $user = $req->query_parameters->{username};
+    my $pass = $req->query_parameters->{password};
+    my $token = $auth->auth($user, $pass);
+    my $res = Plack::Response->new;
+
+    if (!$token) {
+        $res->redirect('/login?badpassword=1');
+        $res->cookies->{s} = '';
+
+        return $res->finalize;
     }
     else {
-        my %args;
-        if (exists $req->query_parameters->{badpassword}) {
-            $args{badpassword} = 1;
-        }
-        my $body = Auth::View->login_page(%args);
-        return [200, ['Content-Type' => 'text/html'], $body ];
+        my $redirect = _redirect_target($req, %config);
+        $res->redirect($redirect);
+        $res->cookies->{s} = {
+            value => $token,
+            expires => time + $config{timeout_sec},
+            secure => 1,
+        };
+        return $res->finalize;
     }
 }
 
@@ -93,34 +102,9 @@ sub newuser
         my $status = $auth->newuser($params->{username}, $params->{password});
         my $res = Plack::Response->new;
         $res->redirect($config{newuserredirect} || '/');
-            
+
         return $res->finalize;
-    } else {
-        return [405, [], 'This is a POST-only URL'];
     }
 }
-
-sub wrap
-{
-    my ($app, %userconfig) = @_;
-
-    my %config = (%defconfig, %userconfig);
-    
-    return sub {
-        my $env = shift;
-
-        require PathDerbyDues::Utilities::DBConnect;
-	my $dbh = PayDerbyDues::Utilities::DBConnect::GetDBH();
-        
-        dispatch($app, $env, $dbh, %config);
-    };
-}
-
-%routes = (
-    '/login' => \&login,
-    '/confirm' => \&confirm,
-    '/newuser' => \&newuser,
-);
-
 
 1;
