3c54baffb26259c2b00283154a6c2c12bf202665
root <root@ip-10-0-1-44.us-west-2.compute.internal>
2015-07-05 21:38:39 +0000

    templatize the nav bar as container for content 	modified:   router.psgi 	modified:   www/handlebarstemplates/feescheduleadmin.hbs 	deleted:    www/handlebarstemplates/foo.hbs 	added:      www/handlebarstemplates/layout.hbs 	modified:   www/handlebarstemplates/learnmore.hbs 	modified:   www/handlebarstemplates/rollout.hbs 	deleted:    www/handlebarstemplates/test.hbs 	modified:   www/handlebarstemplates/who.hbs 	modified:   www/js/app/main.js

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,18 +1,3 @@
-/*
-define(function (require) {
-	// Load any app-specific modules
-	// with a relative require call,
-	// like:
-	var messages = require('./messages');
-
-	// Load library/vendor modules using
-	// full IDs, like:
-	var print = require('print');
-
-	print(messages.getHello());
-});
-*/
-
 define(["jquery", "bootstrap", "datatables"], function($) {
 		$(document).ready(function() {
 			$('table.datatable').dataTable();
