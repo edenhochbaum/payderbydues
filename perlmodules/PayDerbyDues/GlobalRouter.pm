5ed13855190ed1b6ddaf4f2a22c9bc4492cf0418
root <root@ip-10-0-1-102.us-west-2.compute.internal>
2015-08-16 19:26:04 +0000

    remove references to dont_finalize option 	modified:   perlmodules/PayDerbyDues/GlobalRouter.pm

--- perlmodules/PayDerbyDues/GlobalRouter.pm
+++ perlmodules/PayDerbyDues/GlobalRouter.pm
@@ -15,19 +15,19 @@ sub _GetGlobalRouter {
 	$GlobalRouter = Router::Simple->new();
 
 	# REQUIRE AUTHORIZATION #
-	$GlobalRouter->connect('/feescheduleadmin', { func => \&PayDerbyDues::WorkFlows::All::fee_schedule_admin, requires_auth => 1, dont_finalize => 1 });
-	$GlobalRouter->connect('/emailed', { func => \&PayDerbyDues::WorkFlows::All::email_ed, requires_auth => 1, dont_finalize => 1 });
-	$GlobalRouter->connect('/arcady', { func => \&PayDerbyDues::WorkFlows::All::arcady, requires_auth => 1, dont_finalize => 1 });
+	$GlobalRouter->connect('/feescheduleadmin', { func => \&PayDerbyDues::WorkFlows::All::fee_schedule_admin, requires_auth => 1 });
+	$GlobalRouter->connect('/emailed', { func => \&PayDerbyDues::WorkFlows::All::email_ed, requires_auth => 1 });
+	$GlobalRouter->connect('/arcady', { func => \&PayDerbyDues::WorkFlows::All::arcady, requires_auth => 1 });
 
 	# OPEN TO THE WORLD #
-	$GlobalRouter->connect('/rollout', { func => \&PayDerbyDues::WorkFlows::All::rollout, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/who', { func => \&PayDerbyDues::WorkFlows::All::who, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/learnmore', { func => \&PayDerbyDues::WorkFlows::All::learnmore, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/', { func => \&PayDerbyDues::WorkFlows::All::index, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/login', { func => \&PayDerbyDues::WorkFlows::All::login, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/newuser', { func => \&PayDerbyDues::WorkFlows::All::newuser, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/badlogin', { func => \&PayDerbyDues::WorkFlows::All::badlogin, requires_auth => '', dont_finalize => 1 });
-	$GlobalRouter->connect('/goodlogin', { func => \&PayDerbyDues::WorkFlows::All::goodlogin, requires_auth => '', dont_finalize => 1 });
+	$GlobalRouter->connect('/rollout', { func => \&PayDerbyDues::WorkFlows::All::rollout, requires_auth => ''});
+	$GlobalRouter->connect('/who', { func => \&PayDerbyDues::WorkFlows::All::who, requires_auth => ''});
+	$GlobalRouter->connect('/learnmore', { func => \&PayDerbyDues::WorkFlows::All::learnmore, requires_auth => ''});
+	$GlobalRouter->connect('/', { func => \&PayDerbyDues::WorkFlows::All::index, requires_auth => ''});
+	$GlobalRouter->connect('/login', { func => \&PayDerbyDues::WorkFlows::All::login, requires_auth => ''});
+	$GlobalRouter->connect('/newuser', { func => \&PayDerbyDues::WorkFlows::All::newuser, requires_auth => ''});
+	$GlobalRouter->connect('/badlogin', { func => \&PayDerbyDues::WorkFlows::All::badlogin, requires_auth => ''});
+	$GlobalRouter->connect('/goodlogin', { func => \&PayDerbyDues::WorkFlows::All::goodlogin, requires_auth => ''});
 
 	return $GlobalRouter;
 }
