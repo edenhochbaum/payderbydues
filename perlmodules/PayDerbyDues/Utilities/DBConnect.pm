c5a37b4913b8ce3d6342aaea89052510cc8f0708
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2015-09-05 23:21:27 -0700

    Make DBI connect work with or without $ENV{DBHOST}
    If you're running the app locally instead of on EC2, you probably
    don't have your database on a separate server and don't want to pass
    host=whatever to DBI, nor set the DBHOST environment variable. And
    you may as well hardcode the database name to 'payderbydues'.

--- perlmodules/PayDerbyDues/Utilities/DBConnect.pm
+++ perlmodules/PayDerbyDues/Utilities/DBConnect.pm
@@ -6,7 +6,13 @@ use warnings;
 use DBI;
 
 sub GetDBH {
-	return DBI->connect("dbi:Pg:dbname=$ENV{DBNAME};host=$ENV{DBHOST}", $ENV{DBUSERNAME}, $ENV{DBPASSWORD}, {
+	my $connstring;
+	if (exists $ENV{DBHOST}) {
+            $connstring = "dbi:Pg:dbname=$ENV{DBNAME};host=$ENV{DBHOST}";
+        } else {
+            $connstring = "dbi:Pg:dbname=payderbydues";
+        }
+	return DBI->connect($connstring, $ENV{DBUSERNAME}, $ENV{DBPASSWORD}, {
 		AutoCommit => 1,
 		RaiseError => 1,
 		PrintError => 1,
