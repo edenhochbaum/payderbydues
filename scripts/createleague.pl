784e695c48db18d4eb8d2b74e7aafd76d4f9e585
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2015-09-18 20:15:55 -0400

    Add createleauge.pl script
    This creates a new league and adds an entry to the leaguemember table for
    its admin, which must be an existing user.

---
+++ scripts/createleague.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use PayDerbyDues::Utilities::DBConnect;
+use DBI;
+use Getopt::Long;
+
+my ($leaguename, $adminemail);
+
+GetOptions('leaguename=s' => \$leaguename,
+           'adminemail=s' => \$adminemail);
+
+if (!$adminemail || !$leaguename) {
+    print 'Usage: createleague.pl --leaguename "Toaster City" --adminemail bob@example.com\n';
+    exit 1;
+}
+
+my $dbh = PayDerbyDues::Utilities::DBConnect::GetDBH;
+
+my $admins = $dbh->selectcol_arrayref('select id from users where email = ?',
+                                      {}, $adminemail);
+my $userid;
+
+if (@$admins) {
+    $userid = $admins->[0];
+} else {
+    print "User with email $adminemail not found in database\n";
+    exit 2;
+}
+
+my $sth = $dbh->prepare(q{insert into league (name) values (?) returning id});
+$sth->execute($leaguename);
+my $leagueid = $sth->fetch()->[0];
+
+$dbh->do(q{insert into leaguemember (leagueid, userid, roleid)
+                  values (?, ?, ?)}, {}, $leagueid, $userid, 0);
+
