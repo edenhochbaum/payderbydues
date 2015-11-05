54934dcf51fe78df7a19882aceb56979098761d9
Arcady Goldmints-Orlov <crzwdjk@gmail.com>
2015-11-05 18:22:45 -0500

    Add "unit" column to interval, needed for create_invoices.pl
    It gets fed into a postgres function so that we can round the due
    date to the start of the next billing interval

--- scripts/setup.pl
+++ scripts/setup.pl
@@ -9,12 +9,12 @@ my $dbh = PayDerbyDues::Utilities::DBConnect::GetDBH;
 
 # set up the interval table, used to calculate whether customer has been
 # billed recently enough
-my $sth = $dbh->prepare('insert into interval (id, interval) values (?,?)');
-$sth->execute(0, '1000 years');
-$sth->execute(1, '1 year');
-$sth->execute(2, '1 month');
-$sth->execute(3, '1 week');
-$sth->execute(4, '1 day');
+my $sth = $dbh->prepare('insert into interval (id, interval, unit) values (?,?)');
+$sth->execute(0, '1000 years', undef);
+$sth->execute(1, '1 year', 'year');
+$sth->execute(2, '1 month', 'month');
+$sth->execute(3, '1 week', 'week');
+$sth->execute(4, '1 day', 'day');
 
 $sth = $dbh->prepare('insert into role (id, name) values (?, ?)');
 $sth->execute(0, 'League admin');
